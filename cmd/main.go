a0782b51db8e4ec6bcf27444a08dc8e72c241372
Yorukot <hi@yorukot.me>
2025-11-27 09:00:11 +0800

    chore: change port of api

--- cmd/main.go
+++ cmd/main.go
@@ -28,7 +28,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
+// @host localhost:8000
 // @BasePath /api
 // @schemes http https
 
--- utils/config/config.go
+++ utils/config/config.go
@@ -19,7 +19,7 @@ type EnvConfig struct {
 	AppEnv       AppEnv `env:"APP_ENV" envDefault:"prod"`
 	AppName      string `env:"APP_NAME" envDefault:"knocker"`
 	AppMachineID int16  `env:"APP_MACHINE_ID" envDefault:"1"`
-	AppPort      string `env:"APP_PORT" envDefault:"8080"`
+	AppPort      string `env:"APP_PORT" envDefault:"8000"`
 
 	JWTSecretKey   string `env:"JWT_SECRET_KEY,required" envDefault:"change_me_to_a_secure_key"`
 	FrontendDomain string `env:"FRONTEND_DOMAIN" envDefault:"localhost"`
